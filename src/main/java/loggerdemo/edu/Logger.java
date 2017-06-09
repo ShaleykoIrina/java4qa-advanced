46e8fd2f23a80591fa9f28bfd00b088217d61abc
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-09 11:31:49 +0300

    Enum Demo.

--- src/main/java/loggerdemo/edu/Logger.java
+++ src/main/java/loggerdemo/edu/Logger.java
@@ -16,8 +16,41 @@ public class Logger {
      */
     public void log(String message, LoggerMessageFormatter formatter) {
         if (filter.filter(message)) {
+            switch (Sevrity.???) {
+                case 1: ???
+                case 2: ???
+            }
             saver.save(formatter.format(message));
+
         }
     }
 }
 
+final class Severity {
+    public static final Severity CRITICAL = new Severity(1, "CRITICAL");
+    public static final Severity WARN = new Severity(2, "WARN");
+
+    static valueOf() {}
+
+    private int ordinal;
+    private String name;
+
+    Severity(int ordinal, String name) {
+        this.ordinal = ordinal;
+        this.name = name;
+    }
+}
+
+enum Sever {
+    CR(3), WA(5);
+
+    private int my;
+
+    Sever(int my) {
+        this.my = my;
+    }
+
+    public void m() {}
+}
+
+enum SuperPuperServ extends Sever {}
