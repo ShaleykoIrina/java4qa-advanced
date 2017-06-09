0fd1f40752e514332faaebe0311499343ea84c3c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-09 10:59:13 +0300

    Stream API demo.

--- src/main/java/loggerdemo/edu/App.java
+++ src/main/java/loggerdemo/edu/App.java
@@ -1,18 +1,30 @@
 package loggerdemo.edu;
 
-import static loggerdemo.edu.Logger.log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String... args) {
         Logger logger = new Logger(new MessageContentLoggerFilter(), new ConsoleLoggerSaver(), formatter);
-        logger.log("message", message -> null);
+        logger.log("message", message -> "");
+
+        Predicate<String> stringPredicate = element -> element.length() < 6;
+        List<Integer> collect = new ArrayList<String>().parallelStream()
+                .filter(new Object()::equals)
+                .filter(stringPredicate)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
 
         System.out.println("R!");
-        MessageBuilder()
-                .withMessage("R!")
-                .withDecorator(message -> {message + "timestapm"})
-                .withLocale(new LocaleFactory())
-                .withSaver(SaverFactory.createSaver())
-            .build()
+    }
+}
+
+class MyFilters {
+    public static boolean filterByLength(String str) {
+        return str.length() < 10;
     }
 }
--- src/main/java/loggerdemo/edu/Logger.java
+++ src/main/java/loggerdemo/edu/Logger.java
@@ -4,19 +4,17 @@ public class Logger {
     //GRASP: Creator
     private LoggerFilter filter = new FilterFactory().create(); //Factory -> Abstract Factory
     private LoggerSaver saver; //DI
-    private LoggerMessageFormatter formatter;
 
-    public Logger(LoggerFilter filter, LoggerSaver saver, LoggerMessageFormatter formatter) {
+    public Logger(LoggerFilter filter, LoggerSaver saver) {
         this.filter = filter;
         this.saver = saver;
-        this.formatter = formatter;
     }
 
     /**
      * JavaDoc comment. <b>AHTUNG!!!! NOT NULL!!</b>
      * @throws
      */
-    public void log(String message) {
+    public void log(String message, LoggerMessageFormatter formatter) {
         if (filter.filter(message)) {
             saver.save(formatter.format(message));
         }
