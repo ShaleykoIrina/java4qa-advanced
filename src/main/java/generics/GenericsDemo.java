d79f967afd5e22b7f5a9924abafc995b3d4f2c19
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-17 21:39:17 +0300

    Add more complex generics demo.

--- src/main/java/generics/GenericsDemo.java
+++ src/main/java/generics/GenericsDemo.java
@@ -1,21 +1,65 @@
 package generics;
 
-import java.util.Arrays;
-import java.util.Comparator;
+
+import java.io.Serializable;
+import java.util.Collection;
 
 public class GenericsDemo {
     public static void main(String[] args) {
-        MyCollection<Number> myCollection = new MyCollection<>();
-//        myCollection.add();
+        //region showcase #1: diamond operator + autoboxing
+        MyCollection<Number> c1 = new MyCollection<>();
+        c1.add(1);
+        //endregion
 
-        Arrays.<Integer>asList(1,2,3).sort(
-                (o1, o2) -> o1.byteValue()
-        );
+        //region showcase #2: types equality
+        MyCollection<Number> c2 = new MyCollection<Number>();
+        /**
+         * Separate compile-time type system and runtime types!
+         */
+//        MyCollection<Number> c3 = new MyCollection<Integer>();
+//        MyCollection<Integer> c4 = new MyCollection<Number>();
+//        https://stackoverflow.com/questions/2745265/is-listdog-a-subclass-of-listanimal-why-arent-javas-generics-implicitly-p
+        //endregion
+
+        //region showcase #3: more complex type system
+        MyCollection<? extends Number> c5 = new MyCollection<Number>();
+        MyCollection<? extends Number> c6 = new MyCollection<Integer>();
+
+        MyCollection<? super Number> c7 = new MyCollection<Number>();
+        MyCollection<? super Number> c8 = new MyCollection<Object>();
+
+//        MyCollection<?> c9 = new MyCollection<?>();
+        //endregion
+
+        //region showcase #3: generic method
+        MyCollection.addToEach(1)
+                .forEach(e -> e.intValue());
+        //endregion
+
     }
 }
 
-class MyCollection<T extends Number> {
+class MyCollection<T> {
     public void add(T element) {}
-
     public T get() { return null; }
+
+    //region showcase #3: generic method
+    public static <P> Collection<P> addToEach(P arg) { return null; }
+    //endregion
+
+    //region showcase #4: much more complex type system
+    public static <P extends MyCollection & Serializable> P m2() { return null; }
+    //endregion
 }
+
+//region showcase #5: fixing type parameter with inheritance/implementation
+class MySuperCollection1 extends MyCollection {
+    //what's T?
+}
+class MySuperCollection2<Integer> extends MyCollection {
+    //what's T?
+}
+class MySuperCollection3 extends MyCollection<Integer> {
+    //what's T?
+}
+//endregion
