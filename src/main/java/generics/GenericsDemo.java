7d1e5efdc67c70a4ee2752f54bf498ff44eaff97
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-06-21 09:54:26 +0300

    Generic method call demo.

--- src/main/java/generics/GenericsDemo.java
+++ src/main/java/generics/GenericsDemo.java
@@ -2,6 +2,7 @@ package generics;
 
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class GenericsDemo {
@@ -12,28 +13,30 @@ public class GenericsDemo {
         //endregion
 
         //region showcase #2: types equality
-        MyCollection<Number> c2 = new MyCollection<Number>();
+        MyCollection<Number> c2 = new MyCollection<>();
         /**
          * Separate compile-time type system and runtime types!
          */
 //        MyCollection<Number> c3 = new MyCollection<Integer>();
+//        c3.get().intValue();
+
 //        MyCollection<Integer> c4 = new MyCollection<Number>();
 //        https://stackoverflow.com/questions/2745265/is-listdog-a-subclass-of-listanimal-why-arent-javas-generics-implicitly-p
         //endregion
 
         //region showcase #3: more complex type system
-        MyCollection<? extends Number> c5 = new MyCollection<Number>();
+        MyCollection<? extends Number> c5 = new MyCollection<>();
         MyCollection<? extends Number> c6 = new MyCollection<Integer>();
 
-        MyCollection<? super Number> c7 = new MyCollection<Number>();
+        MyCollection<? super Number> c7 = new MyCollection<>();
         MyCollection<? super Number> c8 = new MyCollection<Object>();
 
-//        MyCollection<?> c9 = new MyCollection<?>();
+        MyCollection c9 = new MyCollection<Number>();
         //endregion
 
         //region showcase #3: generic method
-        MyCollection.addToEach(1)
-                .forEach(e -> e.intValue());
+        MyCollection.<Number>addToEach("")
+                .forEach(e -> e.byteValue());
         //endregion
 
     }
@@ -44,7 +47,9 @@ class MyCollection<T> {
     public T get() { return null; }
 
     //region showcase #3: generic method
-    public static <P> Collection<P> addToEach(P arg) { return null; }
+    public static <P> Collection<P> addToEach(String arg) {
+        return null;
+    }
     //endregion
 
     //region showcase #4: much more complex type system
